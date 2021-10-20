aa2bdaa8b987feafafd8b58d67a0f07bd6c2604d
Jamie McMahan <grund23@gmail.com>
2021-10-20 09:41:28 -0500

    update

--- js/kanban.js
+++ js/kanban.js
@@ -1,3 +1,18 @@
+let order = 1;
+let adding = false;
+
+const error = document.querySelector('.error');
+const message = 'Please add a description.';
+
+const add_btn = document.querySelector('.add');
+add_btn.addEventListener('click', () => {
+  const target = document.querySelector('#requested');
+  if (adding == false) {
+    adding = true;
+    target.appendChild(create_item());
+  } else {
+    error.innerHTML = message;
+  }
 });
 
 const create_item = () => {
@@ -5,10 +20,8 @@ const create_item = () => {
   item.classList.add('item');
   item.id = 'item-' + order;
   item.draggable = true;
-  item.addEventListener('dragstart', event => 
-  event.dataTransfer.setData('text', event.target.id));
-  item.addEventListener('dragend', event => 
-  event.dataTransfer.clearData());
+  item.addEventListener('dragstart', event => event.dataTransfer.setData('text', event.target.id));
+  item.addEventListener('dragend', event => event.dataTransfer.clearData());
 
   let input = document.createElement('input');
   item.appendChild(input);
@@ -26,7 +39,6 @@ const create_item = () => {
     }
   });
   item.appendChild(save_btn);
-
   return item;
 };
 
@@ -36,6 +48,5 @@ document.querySelectorAll('.drop').forEach(element => {
     const id = event.dataTransfer.getData('text');
     event.target.appendChild(document.getElementById(id));
   });
-  element.addEventListener('dragover', event => 
-  event.preventDefault());
-}); 
+  element.addEventListener('dragover', event => event.preventDefault());
+});
